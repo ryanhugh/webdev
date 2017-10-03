5ada9a7e2e4409435e82c89cb4bbecdb934bcd25
Ubuntu <ubuntu@ip-172-31-22-100.ec2.internal>
2017-10-03 02:21:41 +0000

    init0

--- hw03/oct_hw_new/microblog/config/prod.exs
+++ hw03/oct_hw_new/microblog/config/prod.exs
@@ -62,3 +62,12 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
+ 
+config :microblog, Microblog.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "microblog_prod",
+  hostname: "localhost",
+  pool_size: 10
+
